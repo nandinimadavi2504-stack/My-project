d5f0cc6f3b08179a94a1b6e6e5d14c146598be02
nandinimadavi2504 <nandinimadavi2504@gmail.com>
2025-08-29 11:44:03 +0530

    Added Triangle Circumference program

--- trianglecircumference.cpp
+++ trianglecircumference.cpp
@@ -1,17 +1,17 @@
-#include <iostream>  // Required for cin, cout
+#include <iostream>
 using namespace std;
 
 int main() {
     int T;
-    cin >> T;  // Number of test cases
+    cin >> T;  // number of test cases
 
-    for (int i = 0; i < T; i++) {
+    while (T--) {
         int L1, L2, L3;
         cin >> L1 >> L2 >> L3;
 
-        // Triangle inequality (using >= as per your problem)
+        // Triangle inequality check (using >=)
         if ((L1 + L2 >= L3) && (L1 + L3 >= L2) && (L2 + L3 >= L1)) {
-            int C = L1 + L2 + L3;
+            int C = L1 + L2 + L3;  // circumference
             cout << C << endl;
         } else {
             cout << -1 << endl;
