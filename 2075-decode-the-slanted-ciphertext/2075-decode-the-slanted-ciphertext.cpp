6ef8c46bb939c458addfdfc3af222042cb0cf1cf
Nandini Babanrao Madavi <nandinimadavi2504@gmail.com>
2026-04-05 12:41:02 +0530

    Time: 23 ms (79.49%), Space: 38.3 MB (81.2%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -6,23 +6,12 @@ public:
         int n = encodedText.size();
         int cols = n / rows;
 
-        vector<vector<char>> mat(rows, vector<char>(cols));
-
-        // Fill matrix row-wise
-        int idx = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                mat[i][j] = encodedText[idx++];
-            }
-        }
-
         string result = "";
 
-        // Traverse diagonally
         for (int j = 0; j < cols; j++) {
             int i = 0, k = j;
             while (i < rows && k < cols) {
-                result += mat[i][k];
+                result += encodedText[i * cols + k];
                 i++;
                 k++;
             }
